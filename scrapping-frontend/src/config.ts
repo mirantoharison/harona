51ed49fcfd8ba26b934558e75c56c09a79ff1e7a
Miranto Harison RANDRIAMAMPIANINA <miranto@eufonie.fr>
2024-11-05 20:17:41 +0300

    Update : vercel.json to handle redirection to external api and update dataprovider url

--- scrapping-frontend/src/config.ts
+++ scrapping-frontend/src/config.ts
@@ -1,6 +1,6 @@
 const getApiUrl = () => {
   // Utilisez `window` pour accéder à des variables globales, ou remplacez par des valeurs d'environnement lors du build.
-  return "https://harona-eight.vercel.app/"; // valeur par défaut pour dev
+  return "https://harona-eight.vercel.app/api"; // valeur par défaut pour dev
 };
 
 export default getApiUrl;
