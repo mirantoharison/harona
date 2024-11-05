2c2c5345a66573cb9a0cdd858553dafb8e6a54d5
Miranto Harison RANDRIAMAMPIANINA <miranto@eufonie.fr>
2024-11-05 19:24:37 +0300

    Add : proxy logics into vercel deployment

--- scrapping-frontend/src/config.ts
+++ scrapping-frontend/src/config.ts
@@ -1,6 +1,6 @@
 const getApiUrl = () => {
   // Utilisez `window` pour accéder à des variables globales, ou remplacez par des valeurs d'environnement lors du build.
-  return "http://3.15.171.43/api"; // valeur par défaut pour dev
+  return "https://harona-eight.vercel.app/"; // valeur par défaut pour dev
 };
 
 export default getApiUrl;
